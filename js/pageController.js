9a95a1f53588da4d74071ed8642d16ac6dc85fd6
HalfSend <adamc.barnard1@gmail.com>
2021-01-10 23:48:11 -0500

    First Commit

---
+++ js/pageController.js
@@ -0,0 +1,16 @@
+function ToAbout()
+{
+    document.getElementById("about").scrollIntoView();
+}
+function ToHome()
+{
+    document.getElementById("home").scrollIntoView();
+}
+function ToProjects()
+{
+    document.getElementById("projects").scrollIntoView();
+}
+function ToContact()
+{
+    document.getElementById("contact").scrollIntoView();
+}
